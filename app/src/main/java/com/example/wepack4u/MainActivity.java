c0c7f912c32b8db9a66cbfbb9efd91f695c0fd74
Brandon Ong <brandon2_ong@mymai.sutd.edu.sg>
2022-03-20 14:36:52 +0800

    Added db_test and Pokemon class Created dummy activity_main to simulate app

--- app/src/main/java/com/example/wepack4u/MainActivity.java
+++ app/src/main/java/com/example/wepack4u/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.wepack4u;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText edit_name = findViewById(R.id.edit_name);
+        final EditText edit_type = findViewById(R.id.edit_type);
+        Button button = findViewById(R.id.btn_submit);
+        db_test db_poke = new db_test();
+        button.setOnClickListener(v -> { //when button is click, do this
+            //design logic
+            Pokemon pokemon = new Pokemon(edit_name.getText().toString(),edit_type.getText().toString());
+            db_poke.add(pokemon).addOnSuccessListener(success ->{
+                Toast.makeText(this, "Record is inserted", Toast.LENGTH_SHORT).show(); //if successful, do this
+            }).addOnFailureListener(error -> {
+                Toast.makeText(this, ""+ error.getMessage(), Toast.LENGTH_SHORT).show(); //if error, do this
+            });
+        });
+
     }
 }
--- app/src/main/java/com/example/wepack4u/db_test.java
+++ app/src/main/java/com/example/wepack4u/db_test.java
@@ -1,15 +1,20 @@
 package com.example.wepack4u;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Draw_Data_test {
+public class db_test {
     //for database
     private DatabaseReference databaseReference;
 
-    public Draw_Data_test(){
+    public db_test(){
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        databaseReference = db.getReference(Pokemon.class);
+        databaseReference = db.getReference(Pokemon.class.getSimpleName()); //not sure why get simplename
+    }
+
+    public Task<Void> add(Pokemon poke_name){ //good practice to do try-catch here so they don't pass in null poke_name
+        return databaseReference.push().setValue(poke_name);
     }
 
 }
