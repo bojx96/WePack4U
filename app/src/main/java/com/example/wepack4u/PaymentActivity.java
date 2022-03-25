913d6adf0478c6dcd57a8a9d858f02ab13b73063
Jaron <jaronho@protonmail.com>
2022-03-25 13:57:17 +0800

    Started coding the table

--- app/src/main/java/com/example/wepack4u/PaymentActivity.java
+++ app/src/main/java/com/example/wepack4u/PaymentActivity.java
@@ -2,11 +2,15 @@ package com.example.wepack4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PaymentActivity extends AppCompatActivity {
     @Override
@@ -14,6 +18,37 @@ public class PaymentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+        // Checkout section
+        ArrayList<FoodItem> cart = new ArrayList<>(); // arraylist should be retrieved from firebase
+        TableLayout cartDisplay = findViewById(R.id.cart);
+        TextView total = findViewById(R.id.total);
+        int counter = 1;
+        double subtotal = 0;
+        // for testing
+        cart.add(new FoodItem("Beef Ramen", 2, 4.70));
+
+        for (FoodItem f : cart) {
+            TableRow trow = new TableRow(this);
+            TextView index = new TextView(this);
+            index.setText(counter+".");
+            trow.addView(index);
+            TextView name = new TextView(this);
+            name.setText(f.getName());
+            trow.addView(name);
+            TextView unit = new TextView(this);
+            unit.setText(f.getUnit());
+            trow.addView(unit);
+            TextView price = new TextView(this);
+            price.setText(f.getPrice());
+            trow.addView(price);
+            cartDisplay.addView(trow);
+            counter++;
+            subtotal = subtotal + f.getPriceValue();
+        }
+
+        total.setText("$"+subtotal);
+
+        // Payment section
         RadioGroup payment_method = findViewById(R.id.payment_method);
         Button checkout = findViewById(R.id.checkout_button);
 
