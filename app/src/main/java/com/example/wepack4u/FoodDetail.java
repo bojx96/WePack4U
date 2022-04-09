ffa01a8f36b3c36c92eb5a7908c6e9fc7100c29e
Brandon Ong <brandon2_ong@mymai.sutd.edu.sg>
2022-04-09 12:14:02 +0800

    added a getFoodDetails, will more later

--- app/src/main/java/com/example/wepack4u/FoodDetail.java
+++ app/src/main/java/com/example/wepack4u/FoodDetail.java
@@ -2,13 +2,26 @@ package com.example.wepack4u;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class FoodDetail extends AppCompatActivity {
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String auth_uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    private String campus;
     RecyclerView foodDetailRecyclerView;
     String options[];
 
@@ -28,4 +41,32 @@ public class FoodDetail extends AppCompatActivity {
     public void toFoodDisplay(View view) {
         startActivity(new Intent(FoodDetail.this, FoodDisplay.class));
     }
+
+    public void getFoodDetails(){
+        DocumentReference userRef = db.collection("users").document(auth_uid);
+        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot userDoc = task.getResult();
+                    if (userDoc.exists()){
+                        campus = (userDoc.getString("campus"));
+//                        foodList(campus);
+                    }
+                    else{
+                        Log.d("get_campus", "does not exist");
+                    }
+                }
+            }
+        });
+    }
+
+//    public void FoodDetails(String campus_name){
+//        db.collection("Campus").whereEqualTo("name", campus_name).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//
+//            }
+//        })
+//    }
 }
