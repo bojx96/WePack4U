9aed687ff6e37128a52348734f1e7be51fe43af6
thatrichan <kevinrichan@gmail.com>
2022-04-12 02:45:37 +0800

    delete item through payment page

--- app/src/main/java/com/example/wepack4u/adaptors/Compost.java
+++ app/src/main/java/com/example/wepack4u/adaptors/Compost.java
@@ -11,6 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wepack4u.R;
 import com.example.wepack4u.utilities.FoodItem;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
 
@@ -18,6 +24,8 @@ public class Compost extends RecyclerView.Adapter<Compost.ViewHolder> {
     private final Context context;
     private final List<FoodItem> cart;
     private final boolean isPayment;
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final String auth_uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     public Compost(Context context, List<FoodItem> cart, boolean isPayment) {
         this.context = context;
@@ -45,6 +53,23 @@ public class Compost extends RecyclerView.Adapter<Compost.ViewHolder> {
         if (isPayment) {
             holder.delete.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
+                    db.collection("users").document(auth_uid).collection("cart").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()){
+                                QuerySnapshot querySnapshot = task.getResult();
+                                for (QueryDocumentSnapshot document: querySnapshot){
+                                    System.out.println("document: "+ document.get("name"));
+                                    System.out.println("foodname: "+ food.getName());
+                                    System.out.println("are they equal? " + document.get("name").equals(food.getName()));
+                                    if (document.get("name").equals(food.getName())){
+                                        db.collection("users").document(auth_uid).collection("cart").document(document.getId()).delete();
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    });
                     cart.remove(position);
                     notifyItemRemoved(position);
                     notifyItemRangeChanged(position, getItemCount());
